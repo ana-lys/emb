13dfe8a0a0ac5eadaea3033f4526180bd684a3b1
ana-lys <shatapdude@gmail.com>
2023-04-26 19:31:55 +0700

    first commit

--- landing_planner/build/catkin_tools_prebuild/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/analys/lidar_ws/src/aruco_detect/build/catkin_tools_prebuild
-# Build directory: /home/analys/lidar_ws/src/aruco_detect/build/catkin_tools_prebuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- landing_planner/build/catkin_tools_prebuild/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/analys/lidar_ws/src/aruco_detect/build/catkin_tools_prebuild/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
